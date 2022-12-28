604ff3572af0f691a86a5ded34bc1c3e8165adb2
Noufal Rahman <noufal24rahman@gmail.com>
2022-12-28 12:27:30 +0530

    added cors

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import os
 from flask import Flask
 from flask_restful import Resource, Api
 from application.db import db
+from flask_cors import CORS
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(current_dir, "db.sqlite3") 
 app.secret_key = "secretkeyforapp"
 db.init_app(app)
